bad7747d9b96d0d3c4fc413132a66ce1be777948
HaeSeung <pyunhs0121@gmail.com>
2025-11-16 23:09:28 -0600

    Enhance MediaAsset structure by adding URL expiration time field and updating comments for clarity

--- backend/internal/core/model/media.go
+++ backend/internal/core/model/media.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 // UploadMediaParams captures the input required to upload a media asset.
 type UploadMediaParams struct {
 	FileName string
@@ -8,13 +10,14 @@ type UploadMediaParams struct {
 	Uploader string
 }
 
-// MediaAsset represents a media object stored in UploadThing.
+// MediaAsset represents a media object stored in the backing media provider.
 type MediaAsset struct {
-	Key      string `json:"key"`
-	URL      string `json:"url"`
-	FileName string `json:"fileName"`
-	MimeType string `json:"mimeType"`
-	Size     int64  `json:"size"`
-	Provider string `json:"provider"`
-	Uploader string `json:"uploader"`
+	Key          string    `json:"key"`
+	URL          string    `json:"url"`
+	URLExpiresAt time.Time `json:"urlExpiresAt,omitempty"`
+	FileName     string    `json:"fileName"`
+	MimeType     string    `json:"mimeType"`
+	Size         int64     `json:"size"`
+	Provider     string    `json:"provider"`
+	Uploader     string    `json:"uploader"`
 }
