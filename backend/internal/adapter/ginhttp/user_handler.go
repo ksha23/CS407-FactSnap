f0f7ce64cc503731d304d541498418c0ad44791c
HaeSeung <pyunhs0121@gmail.com>
2025-12-05 22:56:28 -0600

    Add user statistics endpoint to UserHandler

--- backend/internal/adapter/ginhttp/user_handler.go
+++ backend/internal/adapter/ginhttp/user_handler.go
@@ -1,7 +1,11 @@
 package ginhttp
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/ksha23/CS407-FactSnap/internal/adapter/ginhttp/dto"
 	"github.com/ksha23/CS407-FactSnap/internal/core/port"
 )
 
@@ -14,6 +18,22 @@ func NewUserHandler(userService port.UserService) *UserHandler {
 }
 
 func (h *UserHandler) RegisterRoutes(r *gin.RouterGroup) {
-	// TODO: add API routes for users
-	//questionRoutes := r.Group("/users")
+	userRoutes := r.Group("/users")
+	userRoutes.GET("/stats", h.GetUserStatistics)
+}
+
+func (h *UserHandler) GetUserStatistics(c *gin.Context) {
+	userID := getAuthUserID(c)
+
+	questionCount, responseCount, err := h.UserService.GetUserStatistics(c.Request.Context(), userID)
+	if err != nil {
+		HandleErr(c, fmt.Errorf("%s: %w", "UserHandler::GetUserStatistics", err))
+		return
+	}
+
+	res := dto.GetUserStatisticsRes{
+		QuestionCount: questionCount,
+		ResponseCount: responseCount,
+	}
+	c.JSON(http.StatusOK, res)
 }
